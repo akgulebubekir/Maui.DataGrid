4b68d39a32a6e360611fe166fc1e76fd57171596
Edward Miller <symbiogenesis@outlook.com>
2023-01-30 23:35:41 -0600

    remove unused variables

--- Maui.DataGrid.Sample/MainPage.xaml.cs
+++ Maui.DataGrid.Sample/MainPage.xaml.cs
@@ -11,9 +11,6 @@ public partial class MainPage
     {
         InitializeComponent();
         BindingContext = new MainViewModel();
-
-        var dg = new DataGrid();
-        var dgc = new DataGridColumn();
     }
 }
 
