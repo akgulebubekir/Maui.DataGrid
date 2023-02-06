e66e1aaeeb11fe84ffe3fba75f862672e8c1c545
Edward Miller <symbiogenesis@outlook.com>
2023-02-06 17:34:10 -0600

    don't use expression body

--- Maui.DataGrid/BoolToSelectionModeConverter.cs
+++ Maui.DataGrid/BoolToSelectionModeConverter.cs
@@ -4,8 +4,10 @@ using System.Globalization;
 
 internal sealed class BoolToSelectionModeConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is true ?
-            SelectionMode.Single : SelectionMode.None;
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is true ? SelectionMode.Single : SelectionMode.None;
+    }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
