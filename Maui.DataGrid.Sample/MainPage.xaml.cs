b0a1e610f286b0af6eaa0f576be388f4a02ae4f0
Edward Miller <symbiogenesis@outlook.com>
2023-02-06 02:42:39 -0600

    add column test

--- Maui.DataGrid.Sample/MainPage.xaml.cs
+++ Maui.DataGrid.Sample/MainPage.xaml.cs
@@ -11,6 +11,12 @@ public partial class MainPage
     {
         InitializeComponent();
         BindingContext = new MainViewModel();
+        _addColumnButton1.Clicked += OnAddColumn;
+    }
+
+    private void OnAddColumn(object sender, EventArgs e)
+    {
+        _dataGrid1.Columns.Add(new DataGridColumn() { Title = "Test", Width = new(100) });
     }
 }
 
