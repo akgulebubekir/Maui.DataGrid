9bc140f4dd96cf184cde5354bee0dc07ed08149b
Edward Miller <symbiogenesis@outlook.com>
2023-02-06 12:32:13 -0600

    move converter in sample to its own file and namespace

---
+++ Maui.DataGrid.Sample/Converters/StreakToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace Maui.DataGrid.Sample.Converters;
+
+using System.Globalization;
+using Maui.DataGrid.Sample.Models;
+
+internal class StreakToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Streak s)
+        {
+            return s.Result == Result.Won
+                ? Colors.Green.AddLuminosity(s.NumStreak / 30F)
+                : Colors.Red.AddLuminosity(-s.NumStreak / 30F);
+        }
+        return Colors.Transparent;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
--- Maui.DataGrid.Sample/MainPage.xaml.cs
+++ Maui.DataGrid.Sample/MainPage.xaml.cs
@@ -19,19 +19,3 @@ public partial class MainPage
         _dataGrid1.Columns.Add(new DataGridColumn() { Title = "Test", Width = new(100) });
     }
 }
-
-internal class StreakToColorConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        if (value is Streak s)
-        {
-            return s.Result == Result.Won
-                ? Colors.Green.AddLuminosity(s.NumStreak / 30F)
-                : Colors.Red.AddLuminosity(-s.NumStreak / 30F);
-        }
-        return Colors.Transparent;
-    }
-
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
-}
