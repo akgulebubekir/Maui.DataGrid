ea46255acd6cc21490ee97f75809918a64e451b5
Edward Miller <symbiogenesis@outlook.com>
2023-02-23 16:30:09 -0600

    fix nullability

--- Maui.DataGrid/BoolToSelectionModeConverter.cs
+++ Maui.DataGrid/BoolToSelectionModeConverter.cs
@@ -4,10 +4,10 @@ using System.Globalization;
 
 internal sealed class BoolToSelectionModeConverter : IValueConverter
 {
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return value is true ? SelectionMode.Single : SelectionMode.None;
     }
 
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
 }
