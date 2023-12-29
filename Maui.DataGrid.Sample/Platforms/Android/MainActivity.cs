3a9ce22c1377c4acad56256ac9fd1927438cddc4
Edward Miller <symbiogenisis@outlook.com>
2023-12-29 02:30:03 -0600

    remove unused using

--- Maui.DataGrid.Sample/Platforms/Android/MainActivity.cs
+++ Maui.DataGrid.Sample/Platforms/Android/MainActivity.cs
@@ -2,7 +2,6 @@ namespace Maui.DataGrid.Sample.Platforms.Android;
 
 using global::Android.App;
 using global::Android.Content.PM;
-using global::Android.OS;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
