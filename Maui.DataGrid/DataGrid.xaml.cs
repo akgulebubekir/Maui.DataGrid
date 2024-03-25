0f41a19b72d4433b27fa1dd0a99c5027bef80388
chrigi28 <christian.holinger@gmail.com>
2024-03-25 21:51:46 +0100

    remove this from ctor
    Co-authored-by: Ebu Akgul <ebubekirakgul91@gmail.com>

--- Maui.DataGrid/DataGrid.xaml.cs
+++ Maui.DataGrid/DataGrid.xaml.cs
@@ -51,7 +51,7 @@ public partial class DataGrid
             _collectionView.ItemsSource = InternalItems;
         }
 
-        this.Loaded += (sender, e) =>
+        Loaded += (sender, e) =>
         {
             Reload();
         };
