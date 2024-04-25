a57647fbc02a5eb7f027e652c3fc8e3264b27d16
akgulebubekir <ebubekirakgul91@gmail.com>
2024-04-25 23:08:05 +0200

    Implement selection changed event tests this test added to make sure that #170 has been fixed.

---
+++ Maui.DataGrid.Sample/Tests/SelectionTest.cs
@@ -0,0 +1,65 @@
+namespace Maui.DataGrid.Sample.Tests;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Maui.DataGrid.Sample.Models;
+using Maui.DataGrid.Sample.Tests.TestUtils;
+using Xunit;
+
+public class SelectionTest
+{
+    private readonly List<Team> _teams = Utils.DummyDataProvider.GetTeams();
+
+    [Fact]
+    public async Task BindingSelectedItemTriggersItemSelectedEvent()
+    {
+        var viewModel = new SingleVM<Team>();
+        var datagrid = new DataGrid { ItemsSource = _teams };
+        var eventTriggered = false;
+        var teamToSelect = _teams.ElementAt(5);
+
+        datagrid.ItemSelected += (s, e) =>
+        {
+            eventTriggered = true;
+            Assert.Equal(s, datagrid);
+            Assert.Equal(0, e.PreviousSelection.Count);
+            Assert.Equal(1, e.CurrentSelection.Count);
+            Assert.Equal(new[] { teamToSelect }, e.CurrentSelection);
+        };
+
+        //set a parent to trigger  OnParentSet
+        var parent = new ContentView { Content = datagrid };
+
+        datagrid.SetBinding(DataGrid.SelectedItemProperty, new Binding("Item", source: viewModel));
+
+        viewModel.Item = teamToSelect;
+        Assert.Equal(teamToSelect, await datagrid.GetValueSafe(DataGrid.SelectedItemProperty));
+        Assert.True(eventTriggered);
+    }
+
+    [Fact]
+    public async Task SettingSelectedItemTriggersItemSelectedEvent()
+    {
+        var datagrid = new DataGrid { ItemsSource = _teams };
+        var eventTriggered = false;
+        var teamToSelect = _teams.ElementAt(5);
+
+        datagrid.ItemSelected += (s, e) =>
+        {
+            eventTriggered = true;
+            Assert.Equal(s, datagrid);
+            Assert.Equal(0, e.PreviousSelection.Count);
+            Assert.Equal(1, e.CurrentSelection.Count);
+            Assert.Equal(new[] { teamToSelect }, e.CurrentSelection);
+        };
+
+        //set a parent to trigger  OnParentSet
+        var parent = new ContentView { Content = datagrid };
+
+        datagrid.SelectedItem = teamToSelect;
+        Assert.Equal(datagrid.SelectedItem, teamToSelect);
+        Assert.Equal(teamToSelect, await datagrid.GetValueSafe(DataGrid.SelectedItemProperty));
+        Assert.True(eventTriggered);
+    }
+}
