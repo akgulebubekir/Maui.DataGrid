20681fb3d390d86cf8c035ed9b23e07ce73640ca
Edward Miller <edward.miller@mesirow.com>
2026-07-12 00:13:21 -0500

    add pragma warning to suppress unused MainPage warning in static analysis

--- Maui.DataGrid.Sample/MainPage.xaml.cs
+++ Maui.DataGrid.Sample/MainPage.xaml.cs
@@ -2,6 +2,8 @@ namespace Maui.DataGrid.Sample;
 
 using Maui.DataGrid.Sample.ViewModels;
 
+#pragma warning disable CA1812 // CA1812: MainPage is instantiated in XAML, so it appears unused to static analysis tools.
+
 /// <summary>
 /// Codebehind for the MainPage.
 /// </summary>
